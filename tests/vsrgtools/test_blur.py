aef88830ec5f45e5b17aebca3337129daf95412b
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-05-04 00:45:11 +0200

    update tests

--- tests/vsrgtools/test_blur.py
+++ tests/vsrgtools/test_blur.py
@@ -1,19 +1,27 @@
-from typing import Sequence
+from typing import Sequence, Any, Literal
 
 import pytest
 
-from jetpytools import CustomValueError, to_arr
+from jetpytools import CustomValueError, FuncExceptT, to_arr, norm_display_name
 
-from vsrgtools import bilateral, box_blur, flux_smooth, gauss_blur, median_blur, min_blur, sbr, side_box_blur
+from vsrgtools import BlurMatrix, bilateral, box_blur, flux_smooth, gauss_blur, median_blur, min_blur, sbr, side_box_blur
 from vstools import ConvMode, DitherType, PlanesT, core, depth, get_prop, vs, invert_planes
 
 color_bars = core.colorbars.ColorBars(format=vs.YUV444P12).std.Loop(10)
 
 clip_int8 = depth(color_bars, 8, dither_type=DitherType.NONE)
-clip_int16 = depth(color_bars, 16, dither_type=DitherType.NONE)
+# clip_int16 = depth(color_bars, 16, dither_type=DitherType.NONE)
 clip_fp16 = depth(color_bars, 16, sample_type=vs.FLOAT, dither_type=DitherType.NONE)
 clip_fp32 = depth(color_bars, 32, sample_type=vs.FLOAT, dither_type=DitherType.NONE)
 
+
+def _display_error_msg(clip: vs.VideoNode, func: FuncExceptT, exc: Exception, **kwargs: Any) -> str:
+    assert clip.format
+    return (
+        str(exc) + "\n"
+        + f"{norm_display_name(func)} | <clip format: {clip.format.name}>"
+        + " | <args " + ", ".join(f"{k}={v}" for k, v in kwargs.items()) + " >"
+    )
 
 def _has_not_been_processed(processed: vs.VideoNode, reference: vs.VideoNode, planes: PlanesT, frame_number: int = 0) -> bool:
     inverted_planes = invert_planes(reference, planes)
@@ -30,7 +38,7 @@ def _has_not_been_processed(processed: vs.VideoNode, reference: vs.VideoNode, pl
     return True
 
 
-@pytest.mark.parametrize("clip", [clip_int8, clip_int16, clip_fp16, clip_fp32])
+@pytest.mark.parametrize("clip", [clip_int8, clip_fp16, clip_fp32])
 @pytest.mark.parametrize("radius", [1, [1, 2]])
 @pytest.mark.parametrize("passes", [2])
 @pytest.mark.parametrize(
@@ -43,7 +51,7 @@ def _has_not_been_processed(processed: vs.VideoNode, reference: vs.VideoNode, pl
         ConvMode.TEMPORAL
     ]
 )
-@pytest.mark.parametrize("planes", [None, 0, [0, 1], [1, 2]])
+@pytest.mark.parametrize("planes", [None, 0, [1, 2]])
 def test_box_blur_no_exception(
     clip: vs.VideoNode,
     radius: int | Sequence[int],
@@ -68,9 +76,7 @@ def test_box_blur_no_exception(
         ]):
             pass
     except Exception as e:
-        pytest.fail(
-            f"box_blur raised an exception with clip = {repr(clip)},\nradius={radius}, passes={passes}, mode={mode}, planes={planes}:\n{e}"
-        )
+        pytest.fail(_display_error_msg(clip, box_blur, e, radius=radius, passes=passes, mode=mode, planes=planes))
     else:
         assert isinstance(result, vs.VideoNode)
 
@@ -82,7 +88,7 @@ def test_side_box_blur_no_exception() -> None:
     ...
 
 
-@pytest.mark.parametrize("clip", [clip_int8, clip_int16, clip_fp16, clip_fp32])
+@pytest.mark.parametrize("clip", [clip_int8, clip_fp16, clip_fp32])
 @pytest.mark.parametrize("sigma", [0.5, [0.5, 1.0]])
 @pytest.mark.parametrize("taps", [None, 3])
 @pytest.mark.parametrize(
@@ -124,8 +130,18 @@ def test_gauss_blur_no_exception(
             pass
     except Exception as e:
         pytest.fail(
-            f"gauss_blur raised an exception with clip = {repr(clip)},\n"
-            f"sigma={sigma}, taps={taps}, mode={mode}, planes={planes}:\n{e}"
+            _display_error_msg(
+                clip, gauss_blur, e,
+                sigma=sigma,
+                taps=taps,
+                mode=mode,
+                planes=planes,
+                _fast=_fast
+            )
+        )
+    else:
+        assert isinstance(result, vs.VideoNode)
+        assert _has_not_been_processed(result, clip, planes)
         )
     else:
         assert isinstance(result, vs.VideoNode)
