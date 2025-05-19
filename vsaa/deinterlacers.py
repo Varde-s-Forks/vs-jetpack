41b01572196fead75896f8fd6ec07a6c4b38f5af
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-05-19 22:44:09 +0200

    Revert "Deinterlacer: make transpose private"
    This reverts commit 6c81bc45c9c96a0f7922c5214c6797f45920189a.

--- vsaa/deinterlacers.py
+++ vsaa/deinterlacers.py
@@ -46,7 +46,7 @@ class Deinterlacer(ABC):
         for y in sorted((aa_dir for aa_dir in AADirection), key=lambda x: x.value, reverse=self.transpose_first):
             if direction in (y, AADirection.BOTH):
                 if y == AADirection.HORIZONTAL:
-                    clip = self._transpose(clip)
+                    clip = self.transpose(clip)
 
                 clip = self._deinterlacer_function(
                     clip, self.tff, False, **self.get_deint_args(clip, False, **kwargs)
@@ -56,11 +56,11 @@ class Deinterlacer(ABC):
                     clip = core.std.Merge(clip[::2], clip[1::2])
 
                 if y == AADirection.HORIZONTAL:
-                    clip = self._transpose(clip)
+                    clip = self.transpose(clip)
 
         return clip
 
-    def _transpose(self, clip: vs.VideoNode) -> ConstantFormatVideoNode:
+    def transpose(self, clip: vs.VideoNode) -> ConstantFormatVideoNode:
         return clip.std.Transpose()
 
     def copy(self, **kwargs: Any) -> Self:
@@ -113,12 +113,12 @@ class SuperSampler(Deinterlacer, Scaler, ABC):
                         nshift[x][y] = (nshift[x][y] + (-0.25 if tff else 0.25) * subsampling[x]) * 2 - cloc[x]
 
                 if is_width:
-                    clip = self._transpose(clip)
+                    clip = self.transpose(clip)
 
                 clip = self._deinterlacer_function(clip, tff, True, **self.get_deint_args(clip, True, **kwargs))()
 
                 if is_width:
-                    clip = self._transpose(clip)
+                    clip = self.transpose(clip)
 
         if not self.transpose_first:
             nshift.reverse()
@@ -241,14 +241,14 @@ class EEDI3(SuperSampler, Deinterlacer):
 
         return partial(func, clip, field, dh, **kwargs)
 
-    def _transpose(self, clip: vs.VideoNode) -> ConstantFormatVideoNode:
+    def transpose(self, clip: vs.VideoNode) -> ConstantFormatVideoNode:
         if isinstance(self.sclip, vs.VideoNode):
             self.sclip = self.sclip.std.Transpose()
 
         if isinstance(self.mclip, vs.VideoNode):
             self.mclip = self.mclip.std.Transpose()
 
-        return super()._transpose(clip)
+        return super().transpose(clip)
 
     def get_deint_args(self, clip: vs.VideoNode, dh: bool, **kwargs: Any) -> dict[str, Any]:
         self.vthresh = normalize_seq(self.vthresh, 3)
